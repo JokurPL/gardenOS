5a68c4e729d376b87e23f379995554ed3355ee5b
Mateusz Pietrzak <simcitymrsuperslid@gmail.com>
2021-01-01 22:27:44 +0100

    Cleaning code

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,20 @@
 #include <Arduino.h>
-
 #include <SoftwareSerial.h>
 
-char incoming_value = 0;
+// CONSTANTS
+#define LED 10
 
-SoftwareSerial hc06(2, 3);
+char incoming_value;
+
+SoftwareSerial hc06(2, 3); // 2 - Rx, 3 - Tx | Arduino Rx -> HC Tx # Arduino Tx -> Hc Rx by divider 
 
 void setup() {
   Serial.begin(9600);
-  Serial.println("ENTER AT Commands:");
   hc06.begin(9600);
-  pinMode(10, OUTPUT);
+  
+  Serial.println("ENTER AT Commands:"); 
+  
+  pinMode(LED, OUTPUT); // simple LED
 }
 
 void loop() {
@@ -20,10 +24,29 @@ void loop() {
     incoming_value = hc06.read();
     Serial.write(incoming_value);
     
-    if (incoming_value == 'a' && digitalRead(10) == LOW)
-      digitalWrite(10, HIGH);
-    else if (incoming_value == 'a' && digitalRead(10) == HIGH)
-      digitalWrite(10, LOW);
+    switch (incoming_value)
+    {
+      // simple turn on/off LED
+    case 'a':
+      if (digitalRead(LED) == 1) 
+      {
+        digitalWrite(LED, LOW);
+      }
+      else 
+      {
+        digitalWrite(LED, HIGH);
+      }
+
+      break;
+    
+    default:
+      break;
+    }
+
+    // if (incoming_value == 'a' && digitalRead(10) == LOW)
+    //   digitalWrite(10, HIGH);
+    // else if (incoming_value == 'a' && digitalRead(10) == HIGH)
+    //   digitalWrite(10, LOW);
   }
 
   //Write from Serial Monitor to HC06
